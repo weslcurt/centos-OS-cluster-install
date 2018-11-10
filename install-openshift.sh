72cb8c7ce700143723bd5ddc52d0f844875b510a
weslcurt <18757779+weslcurt@users.noreply.github.com>
2018-11-09 19:03:52 -0600

    Update install-openshift.sh

--- install-openshift.sh
+++ install-openshift.sh
@@ -2,16 +2,51 @@
 
 ## see: https://youtu.be/aqXSbDZggK4
 
-export DOMAIN=${DOMAIN:="$(curl ipinfo.io/ip).nip.io"}
+## Default variables to use
+export INTERACTIVE=${INTERACTIVE:="true"}
+export PVS=${INTERACTIVE:="true"}
+export DOMAIN=${DOMAIN:="$(curl -s ipinfo.io/ip).nip.io"}
 export USERNAME=${USERNAME:="$(whoami)"}
 export PASSWORD=${PASSWORD:=password}
-export VERSION=${VERSION:="v3.11"}
-
-
+export VERSION=${VERSION:="3.11"}
 export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/gshipley/installcentos/master"}
-
 export IP=${IP:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"}
 export API_PORT=${API_PORT:="8443"}
+
+## Make the script interactive to set the variables
+if [ "$INTERACTIVE" = "true" ]; then
+	read -rp "Domain to use: ($DOMAIN): " choice;
+	if [ "$choice" != "" ] ; then
+		export DOMAIN="$choice";
+	fi
+
+	read -rp "Username: ($USERNAME): " choice;
+	if [ "$choice" != "" ] ; then
+		export USERNAME="$choice";
+	fi
+
+	read -rp "Password: ($PASSWORD): " choice;
+	if [ "$choice" != "" ] ; then
+		export PASSWORD="$choice";
+	fi
+
+	read -rp "OpenShift Version: ($VERSION): " choice;
+	if [ "$choice" != "" ] ; then
+		export VERSION="$choice";
+	fi
+	read -rp "IP: ($IP): " choice;
+	if [ "$choice" != "" ] ; then
+		export IP="$choice";
+	fi
+
+	read -rp "API Port: ($API_PORT): " choice;
+	if [ "$choice" != "" ] ; then
+		export API_PORT="$choice";
+	fi 
+
+	echo
+
+fi
 
 echo "******"
 echo "* Your domain is $DOMAIN "
@@ -20,6 +55,9 @@ echo "* Your username is $USERNAME "
 echo "* Your password is $PASSWORD "
 echo "* OpenShift version: $VERSION "
 echo "******"
+
+# install updates
+yum update -y
 
 # install the following base packages
 yum install -y  wget git zile nano net-tools docker-1.13.1\
@@ -31,7 +69,7 @@ yum install -y  wget git zile nano net-tools docker-1.13.1\
 				java-1.8.0-openjdk-headless "@Development Tools"
 
 #install epel
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install epel-release
 
 # Disable the EPEL repository globally so that is not accidentally used during later steps of the installation
 sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
@@ -43,18 +81,21 @@ if [ $? -eq 1 ]; then
 fi
 
 # install the packages for Ansible
-yum -y --enablerepo=epel install ansible pyOpenSSL
+yum -y --enablerepo=epel install pyOpenSSL
+
+curl -o ansible.rpm https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.5-1.el7.ans.noarch.rpm
+yum -y --enablerepo=epel install ansible.rpm
 
 [ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git
 
 cd openshift-ansible && git fetch && git checkout release-${VERSION} && cd ..
 
-#cat <<EOD > /etc/hosts
-#127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
-#::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-#${IP}		$(hostname) console console.${DOMAIN}  
-#EOD
-#this got me further but still not there
+cat <<EOD > /etc/hosts
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
+::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
+${IP}		$(hostname) console console.${DOMAIN}  
+EOD
+
 if [ -z $DISK ]; then 
 	echo "Not setting the Docker storage."
 else
@@ -90,22 +131,55 @@ if [ "$memory" -lt "4194304" ]; then
 	export METRICS="False"
 fi
 
-if [ "$memory" -lt "8388608" ]; then
+if [ "$memory" -lt "16777216" ]; then
 	export LOGGING="False"
 fi
 
-# curl -o inventory.download $SCRIPT_REPO/inventory.ini
-# envsubst < inventory.download > inventory.ini
+curl -o inventory.download $SCRIPT_REPO/inventory.ini
+envsubst < inventory.download > inventory.ini
+
+# add proxy in inventory.ini if proxy variables are set
+if [ ! -z "${HTTPS_PROXY:-${https_proxy:-${HTTP_PROXY:-${http_proxy}}}}" ]; then
+	echo >> inventory.ini
+	echo "openshift_http_proxy=\"${HTTP_PROXY:-${http_proxy:-${HTTPS_PROXY:-${https_proxy}}}}\"" >> inventory.ini
+	echo "openshift_https_proxy=\"${HTTPS_PROXY:-${https_proxy:-${HTTP_PROXY:-${http_proxy}}}}\"" >> inventory.ini
+	if [ ! -z "${NO_PROXY:-${no_proxy}}" ]; then
+		__no_proxy="${NO_PROXY:-${no_proxy}},${IP},.${DOMAIN}"
+	else
+		__no_proxy="${IP},.${DOMAIN}"
+	fi
+	echo "openshift_no_proxy=\"${__no_proxy}\"" >> inventory.ini
+fi
+
+mkdir -p /etc/origin/master/
+touch /etc/origin/master/htpasswd
+
 ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
 ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
 
 htpasswd -b /etc/origin/master/htpasswd ${USERNAME} ${PASSWORD}
 oc adm policy add-cluster-role-to-user cluster-admin ${USERNAME}
 
-systemctl restart origin-master-api
+if [ "$PVS" = "true" ]; then
+
+	curl -o vol.yaml $SCRIPT_REPO/vol.yaml
+
+	for i in `seq 1 200`;
+	do
+		DIRNAME="vol$i"
+		mkdir -p /mnt/data/$DIRNAME 
+		chcon -Rt svirt_sandbox_file_t /mnt/data/$DIRNAME
+		chmod 777 /mnt/data/$DIRNAME
+		
+		sed "s/name: vol/name: vol$i/g" vol.yaml > oc_vol.yaml
+		sed -i "s/path: \/mnt\/data\/vol/path: \/mnt\/data\/vol$i/g" oc_vol.yaml
+		oc create -f oc_vol.yaml
+		echo "created volume $i"
+	done
+	rm oc_vol.yaml
+fi
 
 echo "******"
-
 echo "* Your console is https://console.$DOMAIN:$API_PORT"
 echo "* Your username is $USERNAME "
 echo "* Your password is $PASSWORD "
